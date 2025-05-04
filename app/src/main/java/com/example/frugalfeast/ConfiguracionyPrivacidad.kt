f8962f7b9a0fbb5a3124e4bc58fad88ca2673075
AlejandroC-0609 <johan.carrillof@utadeo.edu.co>
2025-05-04 13:30:58 -0500

    Pequeñas cosas

--- app/src/main/java/com/example/frugalfeast/ConfiguracionyPrivacidad.kt
+++ app/src/main/java/com/example/frugalfeast/ConfiguracionyPrivacidad.kt
@@ -1,6 +1,9 @@
 package com.example.frugalfeast
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ConfiguracionyPrivacidad : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,11 +21,35 @@ class ConfiguracionyPrivacidad : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnIrpo = findViewById<Button>(R.id.button3)
+
+        btnIrpo.setOnClickListener {
+            val intent = Intent(this, Politicadeprivacidad::class.java)
+            startActivity(intent)
+        }
+        val btnIrter = findViewById<Button>(R.id.button4)
+
+        btnIrter.setOnClickListener {
+            val intent = Intent(this, TerminosyCondiciones::class.java)
+            startActivity(intent)
+        }
+        val btnIrsob = findViewById<Button>(R.id.button15)
+
+        btnIrsob.setOnClickListener {
+            val intent = Intent(this, SobreApp::class.java)
+            startActivity(intent)
+        }
+        val btnIrcon = findViewById<Button>(R.id.button15)
+
+        btnIrcon.setOnClickListener {
+            val intent = Intent(this, Contactanos::class.java)
+            startActivity(intent)
+        }
+
         val btnVolver = findViewById<ImageButton>(R.id.button18)
 
         btnVolver.setOnClickListener {
             finish()
         }
-
     }
 }
--- app/src/main/java/com/example/frugalfeast/Contactanos.kt
+++ app/src/main/java/com/example/frugalfeast/Contactanos.kt
@@ -1,34 +1,29 @@
 package com.example.frugalfeast
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Miperfil : AppCompatActivity() {
+class Contactanos : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_miperfil)
+        setContentView(R.layout.activity_contactanos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val imgVolver = findViewById<ImageView>(R.id.imageView47)
+        val imgVolver = findViewById<ImageView>(R.id.imageView19)
 
         imgVolver.setOnClickListener {
             finish()
         }
-        val miImageView = findViewById<ImageView>(R.id.imageView49)
-
-        miImageView.setOnClickListener {
-            val intent = Intent(this, ConfiguracionyPrivacidad::class.java)
-            startActivity(intent)
-        }
-
     }
 }
